f1ecb8c2b70fbf21a8ebb644f4c955e7ade1bee5
Artemii2 <87384405+Artemii2@users.noreply.github.com>
2025-07-01 02:15:11 +0300

    Send thank you image by URL instead of local file

--- handlers.py
+++ handlers.py
@@ -204,13 +204,12 @@ def handle_confirmation(update: Update, context: CallbackContext) -> int:
                         parse_mode='HTML'
                     )
                     logger.info("Message successfully sent to group")
-                    # Отправляем картинку с благодарностью
-                    with open('images/thank_you.jpg', 'rb') as photo:
-                        context.bot.send_photo(
-                            chat_id=query.message.chat_id,
-                            photo=photo,
-                            caption="💖 Спасибо за ваш отзыв! Мы ценим ваше мнение!"
-                        )
+                    # Отправляем картинку с благодарностью по URL
+                    context.bot.send_photo(
+                        chat_id=query.from_user.id,
+                        photo="https://downloader.disk.yandex.ru/preview/0c87205a937244ea6a8fc5922f23b0509480148a7623eca8d9aa768eaa88d0fe/68635352/DQPz_uzFBQFbC85Azw-c2xGnqlheV9deujryIfiG6pN0Cq-APupSFdcd7maJCLF8H50_LYGaZ0SRIP2e-6HsuA%3D%3D?uid=0&filename=a8d5e7c2-3262-4b5e-b0b2-a2c654b38a89.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v3&size=2048x2048",
+                        caption="💖 Спасибо за ваш отзыв! Мы ценим ваше мнение!"
+                    )
                 else:
                     logger.error(f"Группа {OWNER_CHAT_ID} недоступна для бота")
                     query.edit_message_text(
